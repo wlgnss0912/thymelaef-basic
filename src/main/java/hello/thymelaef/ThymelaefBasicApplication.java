cf9955c3b06c0e039825f592a7954364d102a5ce
jjuun <jeonjihoon93@naver.com>
2022-04-26 00:06:20 +0900

    스프링 MVC 2 시작

---
+++ src/main/java/hello/thymelaef/ThymelaefBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymelaef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelaefBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelaefBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/thymelaef/ThymelaefBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymelaef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymelaefBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
